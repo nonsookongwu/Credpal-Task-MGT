a63543799392aa01439dd73b9510b6f66036c3e7
nonsookongwu <nonso.okongwu@gmail.com>
2024-06-08 18:32:57 +0100

    "Updated global prefix to 'api/v1', added signup endpoint to UsersController and UsersService, and injected UserEntity repository into UsersService"

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private usersRepository: Repository<UserEntity>,
+  ) {}
+
+  async signup(body: any) {
+    // console.log(body)
+    const user = this.usersRepository.create(body)
+    return await this.usersRepository.save(user)
+  }
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
